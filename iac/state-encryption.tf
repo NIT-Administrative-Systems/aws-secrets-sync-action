2ff10e6a0f668e4330541c79458a69f686885d2c
Casey Colby <cecolby11@users.noreply.github.com>
2025-10-21 13:49:43 -0500

    post  migration steps

--- iac/state-encryption.tf
+++ iac/state-encryption.tf
@@ -24,26 +24,11 @@ terraform {
             keys = key_provider.pbkdf2.local_state_passphrase
         }
 
-        method "unencrypted" "migrate" {
-            # Used as a fallback for the initial state of a previously-unencrypted project.
-            # This does not need to be included for brand-new projects that start out encrypted.
-        }
-
         # This is YOUR module's state.
         state {
             method = method.aes_gcm.local-state-encryption
 
-            # Only needed for a pre-existing Terraform project, which has an unencrypted state file.
-            # Once this has run for every environment, the fallback can be removed, and the
-            # enforced = true option can be enabled.
-            fallback {
-                method = method.unencrypted.migrate
-            }
-
-            # Enable this when all envs have been moved to encrypted state. It prevents accidentally writing
-            # an unencrypted state file, if the passphrase var is unset for some reason.
-            #
-            # enforced = true
+            enforced = true
         }
     }
 }
